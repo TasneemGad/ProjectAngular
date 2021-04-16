2cd998ecc2e0e2b76985927a624c1325537ba1b8
TasneemGad <tasneemgad38@gmail.com>
2021-04-16 02:24:49 +0200

    third

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { NavabarComponent } from './navabar/navabar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { PaymentComponent } from './payment/payment.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductComponent,
+    ProductDetailsComponent,
+    NavabarComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    PaymentComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
