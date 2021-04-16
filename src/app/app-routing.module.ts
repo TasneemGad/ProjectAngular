1ec3b97428df780543568bd5576515498671f250
Shreen Mohamaden <shreen.mohamaden1997@gmail.com>
2021-04-17 01:42:38 +0200

    2ndDay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { OrderComponent } from './order/order.component';
+import { ProductComponent } from './product/product.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"Home" , component:ProductComponent},
+  {path:"Order" , component:OrderComponent},
+  {path:"Cart" , component:CartComponent},
+  {path:"Register" , component:RegisterComponent},
+  {path:"Login" , component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
